dddd0968f4c8662796e9907e83556ac7cfab80c6
GayatrixAnand <anandsharma788778@gmail.com>
2025-04-27 21:05:37 +0530

    Update clone.py

--- PROMUSIC/plugins/tools/clone.py
+++ PROMUSIC/plugins/tools/clone.py
@@ -25,7 +25,7 @@ from PROMUSIC.utils.decorators.language import language
 from datetime import datetime
 CLONES = set()
 
-C_BOT_DESC = "Wᴀɴᴛ ᴀ ʙᴏᴛ ʟɪᴋᴇ ᴛʜɪs? Cʟᴏɴᴇ ɪᴛ ɴᴏᴡ! ✅\n\nVɪsɪᴛ: @ll_ISTKHAR_BABY_lll ᴛᴏ ɢᴇᴛ sᴛᴀʀᴛᴇᴅ!\n\n - Uᴘᴅᴀᴛᴇ: @THUNDERDEVS\n - Sᴜᴘᴘᴏʀᴛ: @THUNDERDEVS"
+C_BOT_DESC = "Wᴀɴᴛ ᴀ ʙᴏᴛ ʟɪᴋᴇ ᴛʜɪs? Cʟᴏɴᴇ ɪᴛ ɴᴏᴡ! ✅\n\nVɪsɪᴛ: @KALAKAAR5 ᴛᴏ ɢᴇᴛ sᴛᴀʀᴛᴇᴅ!\n\n - Uᴘᴅᴀᴛᴇ: @THUNDERDEVS\n - Sᴜᴘᴘᴏʀᴛ: @THUNDERDEVS"
 
 C_BOT_COMMANDS = [
                 {"command": "/start", "description": "sᴛᴀʀᴛs ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ"},
