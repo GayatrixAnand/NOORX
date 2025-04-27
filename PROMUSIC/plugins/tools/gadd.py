9b4b12dc540829271a714916c12adbaff2d659e9
GayatrixAnand <anandsharma788778@gmail.com>
2025-04-27 21:01:09 +0530

    Update gadd.py

--- PROMUSIC/plugins/tools/gadd.py
+++ PROMUSIC/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7995626429"
+OWNERS = "890275081"
 from PROMUSIC import app
 from PROMUSIC.utils.database import add_served_chat, get_assistant
 
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -100155005435:
+            if dialog.chat.id == -1002362839740:
                 continue
             try:
 
